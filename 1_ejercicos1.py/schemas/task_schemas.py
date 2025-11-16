d0062836e20edca2f0ee33661699253dec0634fb
Pisfil <ronny_pm@hotmail.com>
2025-11-16 01:50:47 -0500

    Subiendo  ejercicios fastapi

--- 1_ejercicos1.py/schemas/task_schemas.py
+++ 1_ejercicos1.py/schemas/task_schemas.py
@@ -3,15 +3,48 @@ from datetime import date
 
 
 class TaskCreate(BaseModel):
-    title: str = Field(..., min_length=3)
-    description: str = Field(..., min_length=1, max_length=50)
+    title: str = Field(..., max_length=30)
+    description: str = Field(..., min_length=10, max_length=100)
     priority: int = Field(ge=1, le=5)
     due_date: date
 
+    @field_validator("title")
+    @classmethod
+    def validate_title(cls, value):
+        cleaned = value.strip()
+        if cleaned < 3:
+            raise ValueError("Titulo muy corto")
+        return cleaned
+
+    @field_validator("description")
+    @classmethod
+    def validate_description(cls, value):
+        cleaned = value.strip()
+        if len(cleaned) < 10:
+            raise ValueError(
+                "La descripción debe tener al menos 10 caracteres (sin contar espacios)")
+        if len(cleaned) > 100:
+            raise ValueError(
+                "La descripción no puede exceder 100 caracteres")
+        return cleaned
+
     @field_validator("due_date")
     @classmethod
-    def validator_due_date(cls, value, info):
+    def validator_due_date(cls, value):
         if value < date.today():
             raise ValueError(
                 "La fecha de entrega no puede ser anterior a la actual")
         return value
+
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    priority: int
+    due_date: date
+    completed: bool
+
+
+class TaskUpdatePriority(BaseModel):
+    priority: int = Field(ge=1, le=5)
